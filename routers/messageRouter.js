af326bb1b147fe4a2112569a0ce6956b49e66e5b
Rakshanda <rakshanda.mahajan1101@gmail.com>
2020-12-27 14:44:22 +0530

    Api to get All Messages For Convo

--- routers/messageRouter.js
+++ routers/messageRouter.js
@@ -15,17 +15,36 @@ const msg = {
     to: "Ava",
     from: "Dragon",
     message: "Hello",
-    type: "Video"
+    type: "Video",
+    conversationId: "hjhjhj"
 }
 
 app.get('/write', async (req, res, next) => {
     const msgObj = new Message(msg);
     await msgObj.save(err => {
-        if (err)
+        if (err) {
             console.log('Error Occured', err);
+            res.status(404).send(err);
+        }
         else
             res.send("Done")
     });
+})
+
+app.get('/messages', async (req, res) => {
+    let from = req.query.from;
+    let to = req.query.to;
+    let conversationId = from.localeCompare(to) === 1 ? `${to}$${from}` :
+        `${from}$${to}`;
+    Message.query("conversationId").eq(conversationId).using("Conversation-Index").sort("descending").exec((err, result) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
 })
 
 app.get('/read', async (req, res, next) => {
